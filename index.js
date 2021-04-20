4fd23b252c49f24afcc57239cd6b62ad64d4277a
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-20 03:28:09 -0500

    Completed manager questions

--- index.js
+++ index.js
@@ -4,19 +4,52 @@ const inquirer = require('inquirer')
 
 inquirer
     .prompt([
+{ //prompted to enter name, employee id, email addy, office number
+    type: "input",
+    message: "Enter the team manager's name:",
+    name: "mgrName",
+},
 {
     type: "input",
-    message: "What is your name?",
-    name: "name",
+    message: "Enter the team manager's id:",
+    name: "mgrId",
+},
+{
+    type: "input",
+    message: "Enter team manager's email:",
+    name: "mgrEmail",
+},
+{
+    type: "input",
+    message: "Enter the team manager's office number:",
+    name: "mgrNumber"
 },
 {
     type: "list",
-    message: "What employee position do you want to add?",
+    message: "Select the employee position to add:",
     name: "position",
-    choices: ["Intern", "Engineer", "Manager", "Done"]
+    choices: ["Intern", "Engineer", "Done"]
 }])
 .then((answers) => {
 console.log(answers)
+
+// switch to show which questions user is prompted to do
+
+switch (answers.position) {
+    case 'Intern':
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Enter the intern's name:",
+
+            }
+        ])
+        break;
+    case 'Engineer':
+
+        break;
+    default:
+}
 });
 
     
