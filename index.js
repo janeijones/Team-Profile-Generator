d7d27712807cf67d382a32f304afd2a23e1a0e79
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-21 04:46:23 -0500

    Added heading.js file, contains header for HTML file, connected with index.js file. Heading file written to with user input

--- index.js
+++ index.js
@@ -5,154 +5,182 @@ const Employee = require('./lib/Employee.js')
 const Intern = require('./lib/Intern.js')
 const Engineer = require('./lib/Engineer.js')
 const Manager = require('./lib/Manager.js')
+const heading = require('./src/heading.js')
 const employees = [];
 
-inquirer
-    .prompt([
-        { //prompted to enter name, employee id, email addy, office number
+const questions = ([
+    { //prompted to enter name, employee id, email addy, office number
+        type: "input",
+        message: "Enter the team manager's name:",
+        name: "mgrName",
+    },
+    {
+        type: "input",
+        message: "Enter the team manager's ID:",
+        name: "mgrId",
+    },
+    {
+        type: "input",
+        message: "Enter team manager's email:",
+        name: "mgrEmail",
+    },
+    {
+        type: "input",
+        message: "Enter the team manager's office number:",
+        name: "mgrNumber"
+    },
+    {
+        type: "list",
+        message: "Select the employee position to add:",
+        name: "newTeamMember",
+        choices: ["Intern", "Engineer", "Exit"]
+    }]);
+
+
+const askIntQuestions = () => {
+
+    inquirer.prompt([
+        {
             type: "input",
-            message: "Enter the team manager's name:",
-            name: "mgrName",
+            message: "Enter the intern's name:",
+            name: "intName",
         },
         {
             type: "input",
-            message: "Enter the team manager's ID:",
-            name: "mgrId",
+            message: "Enter the intern's ID:",
+            name: "intId",
         },
         {
             type: "input",
-            message: "Enter team manager's email:",
-            name: "mgrEmail",
+            message: "Enter the intern's email:",
+            name: "intEmail",
         },
         {
             type: "input",
-            message: "Enter the team manager's office number:",
-            name: "mgrNumber"
+            message: "Enter the intern's school:",
+            name: "intSchool",
         },
         {
             type: "list",
             message: "Select the employee position to add:",
             name: "newTeamMember",
             choices: ["Intern", "Engineer", "Exit"]
-        }])
-    .then((answers) => {
-        console.log(answers)
-        const manager = new Manager(answers.mgrName, answers.mgrId, answers.mgrEmail, answers.mgrNumber)
-        employees.push(manager)
-        // switch to show which questions user is prompted to do
+        },
+    ])
+        .then((internAnswers) => {
 
-        const askIntQuestions = () => {
-
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Enter the intern's name:",
-                    name: "intName",
-                },
-                {
-                    type: "input",
-                    message: "Enter the intern's ID:",
-                    name: "intId",
-                },
-                {
-                    type: "input",
-                    message: "Enter the intern's email:",
-                    name: "intEmail",
-                },
-                {
-                    type: "input",
-                    message: "Enter the intern's school:",
-                    name: "intSchool",
-                },
-                {
-                    type: "list",
-                    message: "Select the employee position to add:",
-                    name: "newTeamMember",
-                    choices: ["Intern", "Engineer", "Exit"]
-                },
-            ])
-                .then((internAnswers) => {
-
-                    if (internAnswers.newTeamMember === "Intern") {
-                        askIntQuestions();
-                    } else if (internAnswers.newTeamMember === "Engineer") {
-                        askEngQuestions();
-                    } else {
-                        console.log(employees);// generateTheHTML(); 
-                    }
-                    const intern = new Intern(internAnswers.intName, internAnswers.intId, internAnswers.intEmail, internAnswers.intSchool);
-                    employees.push(intern)
-
-                })
-
-        }
-
-        const askEngQuestions = () => {
-
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Enter the engineer's name:",
-                    name: "engName",
-                },
-                {
-                    type: "input",
-                    message: "Enter the engineer's ID:",
-                    name: "engId",
-                },
-                {
-                    type: "input",
-                    message: "Enter the engineer's email:",
-                    name: "engEmail",
-                },
-                {
-                    type: "input",
-                    message: "Enter the engineer's GitHub username:",
-                    name: "engGitHub",
-                },
-                {
-                    type: "list",
-                    message: "Select the employee position to add:",
-                    name: "newTeamMember",
-                    choices: ["Intern", "Engineer", "Exit"]
-                },
-            ])
-                .then((engAnswers) => {
-
-                    if (engAnswers.newTeamMember === "Intern") {
-                        askIntQuestions();
-                    } else if (engAnswers.newTeamMember === "Engineer") {
-                        askEngQuestions();
-                    } else {
-                        console.log(employees);// generateTheHTML(); 
-                    }
-                    const engineer = new Engineer(engAnswers.engName, engAnswers.engId, engAnswers.engEmail, engAnswers.engGitHub);
-                    employees.push(engineer)
-
-                })
-
-        }
-    });
-
-
-        function init() {
-
-        
-
-        switch (answers.newTeamMember) {
-            case 'Intern':
+            if (internAnswers.newTeamMember === "Intern") {
                 askIntQuestions();
-                break;
-            case 'Engineer':
+            } else if (internAnswers.newTeamMember === "Engineer") {
                 askEngQuestions();
-                break;
-            default:
-                console.log(employees);
-            // generateTheHTML();
-        }
+            } else {
+                console.log(employees);// generateTheHTML(); 
+            }
+            const intern = new Intern(internAnswers.intName, internAnswers.intId, internAnswers.intEmail, internAnswers.intSchool);
+            employees.push(intern)
 
-    }
-    
+        })
+
+}
+
+const askEngQuestions = () => {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the engineer's name:",
+            name: "engName",
+        },
+        {
+            type: "input",
+            message: "Enter the engineer's ID:",
+            name: "engId",
+        },
+        {
+            type: "input",
+            message: "Enter the engineer's email:",
+            name: "engEmail",
+        },
+        {
+            type: "input",
+            message: "Enter the engineer's GitHub username:",
+            name: "engGitHub",
+        },
+        {
+            type: "list",
+            message: "Select the employee position to add:",
+            name: "newTeamMember",
+            choices: ["Intern", "Engineer", "Exit"]
+        },
+    ])
+        .then((engAnswers) => {
+
+            if (engAnswers.newTeamMember === "Intern") {
+                askIntQuestions();
+            } else if (engAnswers.newTeamMember === "Engineer") {
+                askEngQuestions();
+            } else {
+                console.log(employees);// generateTheHTML(); 
+            }
+            const engineer = new Engineer(engAnswers.engName, engAnswers.engId, engAnswers.engEmail, engAnswers.engGitHub);
+            employees.push(engineer)
+            // generateHTML(employees);
+
+        //     fs.appendFileSync('./dist/sample.html', `<div class="col-6">
+        //     <div class="card mx-auto mb-3" style="width: 18rem">
+        //     <h5 class="card-header">${engineer.getName()}<br /><br />Engineer</h5>
+        //     <ul class="list-group list-group-flush">
+        //         <li class="list-group-item">ID: ${engineer.getId()}</li>
+        //         <li class="list-group-item">Email Address: ${engineer.getEmail()}</li>
+        //         <li class="list-group-item">Github: ${engineer.getGithub()}</li>
+        //     </ul>
+        //     </div>
+        //     </div>`, function (error) {
+        //         console.log(error)
+        //     })
+
+        // })
+
+});
+}
+// const createManager = (manager) => {
+//     fs.appendFileSync('./dist/sample.html', `<div class="col-6">
+//     <div class="card mx-auto mb-3" style="width: 18rem">
+//     <h5 class="card-header">${manager.getName()}<br /><br />Manager</h5>
+//     <ul class="list-group list-group-flush">
+//         <li class="list-group-item">ID: ${manager.getId()}</li>
+//         <li class="list-group-item">Email Address: ${manager.getEmail()}</li>
+//         <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+//     </ul>
+//     </div>
+// </div>`, function (error) {
+// console.log(error)
+// })
+// }
 
 
+function init() {
+    inquirer.prompt(questions)
+        .then((mgrAnswers) => {
+            const manager = new Manager(mgrAnswers.mgrName, mgrAnswers.mgrId, mgrAnswers.mgrEmail, mgrAnswers.mgrNumber)
+            employees.push(manager);
+            heading();
 
+
+            switch (mgrAnswers.newTeamMember) {
+                case 'Intern':
+                    askIntQuestions();
+                    break;
+                case 'Engineer':
+                    askEngQuestions();
+                    break;
+                default:
+                    console.log(employees);
+                // generateHTML(employees);
+            }
+
+        })
+
+}
+
+init();
