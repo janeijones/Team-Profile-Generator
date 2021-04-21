a93c4bb711d9a82bfa6afef93918f2588783aab1
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-21 06:12:45 -0500

    Test Added: getName, returns name

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,3 +1,4 @@
+const { it } = require('@jest/globals');
 const Employee = require('../lib/Employee.js')
 
 
@@ -5,15 +6,22 @@ describe('Employee', () => {
 
         it("should be created", () => {
             const obj = new Employee()
-            expect(typeof (obj)).toBe('object');
+            expect(typeof (obj)).toBe('object')
         })
 
 })
 
 describe('name', () => {
     it('should be created accepting a string argument', () => {
-        const name = "Janei";
+        const name = "Janei"
         const obj = new Employee(name)
         expect(obj.name).toBe(name)
     })
+
+    it('should return name when called', () => {
+        const name = "Janei"
+        const obj = new Employee(name)
+        expect(obj.getName()).toBe(name)
+    })
 })
+
