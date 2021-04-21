7b4cba3536542ddb3c58597727cd2f1b591ad9a5
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-21 06:13:38 -0500

    Added getId and getEmail functions to employee test - passed

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,27 +1,41 @@
-const { it } = require('@jest/globals');
 const Employee = require('../lib/Employee.js')
 
 
 describe('Employee', () => {
 
-        it("should be created", () => {
-            const obj = new Employee()
-            expect(typeof (obj)).toBe('object')
+    it("should be created", () => {
+        const obj = new Employee()
+        expect(typeof (obj)).toBe('object')
+    })
+
+    describe("getName", () => {
+        it('should return name when called', () => {
+            const name = "Test"
+            const obj = new Employee(name)
+            expect(obj.getName()).toBe(name)
+
+        })
+    });
+
+    describe("getId", () => {
+        it('should return id when called', () => {
+            const id = "2"
+            const obj = new Employee("Test", id)
+            expect(obj.getId()).toBe(id)
         })
 
-})
+    });
 
-describe('name', () => {
-    it('should be created accepting a string argument', () => {
-        const name = "Janei"
-        const obj = new Employee(name)
-        expect(obj.name).toBe(name)
-    })
+    describe("getEmail", () => {
+        it('should return email when called', () => {
+            const email = "test@test.com"
+            const id = "2"
+            const obj = new Employee("Test", id, email)
+            expect(obj.getId()).toBe(id)
+        })
 
-    it('should return name when called', () => {
-        const name = "Janei"
-        const obj = new Employee(name)
-        expect(obj.getName()).toBe(name)
-    })
+    });
+
+
 })
 
