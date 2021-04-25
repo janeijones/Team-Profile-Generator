8ad4ca683e5e030064cf03363de7018835d88ffa
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-24 21:56:06 -0500

    All tests passed

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,6 +4,19 @@ describe("Manager", () => {
         const obj = new Manager()
         expect(typeof(obj)).toBe('object')
     })
-   
+    describe("getNumber", () => {
+        it("should return the office number string", () =>{
+            const officeNumber = "8175557854";
+            const obj = new Manager("Test Name", 1, "Test Email", officeNumber)
+            expect(obj.getNumber()).toBe(officeNumber)
+        })
+    })
+    describe("getRole",()=> {
+        it("should return the manager string", () =>{
+            const officeNumber = "8175557854"
+            const obj = new Manager("Test Name", 1,"Test Email", officeNumber)
+            expect(obj.getRole()).toBe("Manager")
+        })
+    });
     
 })
